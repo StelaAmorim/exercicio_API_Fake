90deabc00c3cb2b18fc6161242039d78a32e389f
StelaAmorim <144139790+StelaAmorim@users.noreply.github.com>
2023-09-05 16:04:54 -0300

    API Filmes
    Exercício de API - Filmes

---
+++ script.js
@@ -0,0 +1,108 @@
+//Velozes e Furiosos 9
+function VelozesFuriosos(){
+    const myJSON = `[
+        "Velozes e Furiosos 9", "Dominic Toretto e Letty vivem uma vida pacata ao lado do filho. Mas eles logo são ameaçados pelo passado de Dom: seu irmão desaparecido Jakob, que retorna e está trabalhando ao lado de Cipher. Cabe a Dom reunir a equipe novamente para enfrentá-los.", "Ação", " 24 de junho de 2021 (Brasil).",
+        "<iframe width='400' height='300' src='https://www.youtube.com/embed/NnDGWyfP7q4' title='Velozes &amp; Furiosos 9 – Trailer Oficial (Universal Pictures) HD' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+    ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+    
+}
+
+//Homem-Aranha: Sem Volta para Casa
+function HomemAranha(){
+    const myJSON = `[
+        "Homem-Aranha: Sem Volta para Casa", "Peter Parker tem a sua identidade secreta revelada e pede ajuda ao Doutor Estranho. Quando um feitiço para reverter o evento não sai como o esperado, o Homem-Aranha e seu companheiro dos Vingadores precisam enfrentar inimigos de todo o multiverso.", "Aventura", "16 de dezembro de 2021 (Brasil).",
+        "<iframe width='400' height='300' src='https://www.youtube.com/embed/CyiiEJRZjSU' title='Homem-Aranha: Sem Volta Para Casa | Trailer Oficial Dublado | 16 de dezembro nos cinemas' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+        ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+}
+    
+
+//Alerta Máximo
+function AlertaMaximo(){
+    const myJSON = `[
+        "Alerta Máximo", "O piloto Brodie Torrance salva seus passageiros de um raio pousando em uma ilha. Os moradores rebeldes e perigosos do local fazem a tripulação refém e Torrance procura ajuda de um passageiro acusado de assassinato.", "Suspense", "26 de janeiro de 2023 (Brasil).",
+        "<iframe width='1280' height='720' src='https://www.youtube.com/embed/hVXP3xUjbb0' title='Alerta Máximo |  Trailer Legendado' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+    ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+}
+//Projeto Adam
+function ProjetoAdam(){
+    const myJSON = `[
+        "O Projeto Adam", "Um viajante do tempo volta ao ano de 2022 por acidente e acaba se encontrando com seu eu mais novo. Juntos, e sendo caçados por forças do futuro, os dois embarcam em uma missão para consertar a linha temporal e salvar seus entes queridos.", "Ficção Científica", "28 de fevereiro de 2022 (Mundial).",
+        "<iframe width='400' height='300' src='https://www.youtube.com/embed/miycJHABS1w' title='O Projeto Adam | Trailer oficial | Netflix' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+    ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+}
+    
+//Invocação do Mal
+function InvocacaoMal(){
+    const myJSON = `[
+        "Invocação do Mal", "Os investigadores paranormais Ed e Lorraine Warren trabalham para ajudar a família aterrorizada por uma entidade demoníaca em sua fazenda.", "Terror", "13 de setembro de 2013 (Brasil).",
+        "<iframe width='400' height='285' src='https://www.youtube.com/embed/GQrrXceHn2E' title='Invocação do Mal - Trailer 1 (leg) [HD] | 13 de setembro nos cinemas' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+    ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+}
+
+//Mistério no Mediterrâneo
+function MisterioMediterraneo(){
+    const myJSON = `[
+        "Mistério no Mediterrâneo", "Em uma incrível viagem à Europa, um policial de Nova York e sua esposa cabeleireira tentam solucionar um caso de assassinato a bordo do iate de um bilionário.", "Comédia/Mistério", "14 de junho de 2019 (mundial).",
+        "<iframe width='1280' height='720' src='https://www.youtube.com/embed/ZewCCWbPlsY' title='Mistério no Mediterrâneo | Trailer | Netflix' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>"
+    ]`;
+    console.log(myJSON)
+
+    const myArray = JSON.parse(myJSON);
+    console.log(myArray)
+
+    document.getElementById("nome").innerHTML= myArray[0]
+    document.getElementById("sinopse").innerHTML= myArray[1]
+    document.getElementById("genero").innerHTML= myArray[2]
+    document.getElementById("ano").innerHTML= myArray[3]
+    document.getElementById("trailer").innerHTML= myArray[4]
+}
